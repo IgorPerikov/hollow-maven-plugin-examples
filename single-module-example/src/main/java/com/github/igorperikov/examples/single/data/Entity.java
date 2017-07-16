4138d019fccf59ecf331fe5ec797b0716991a3c1
IgorPerikov <perikov.igor@gmail.com>
2017-07-16 19:56:26 +0300

    update plugin versions

--- single-module-example/src/main/java/com/github/igorperikov/examples/single/data/Entity.java
+++ single-module-example/src/main/java/com/github/igorperikov/examples/single/data/Entity.java
@@ -1,7 +1,10 @@
 package com.github.igorperikov.examples.single.data;
 
+import com.netflix.hollow.core.write.objectmapper.HollowPrimaryKey;
+
 import java.util.List;
 
+@HollowPrimaryKey(fields = {"intValue"})
 public class Entity {
     private Integer intValue;
     private List<Long> listOfLongValues;
